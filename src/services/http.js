a5c13c5b53d5c06618364c2c01d2e98a38bc94b8
h_latifi8 <h.latifi03@gmail.com>
2021-09-06 18:19:18 +0430

    axios.interceptors

--- src/services/http.js
+++ src/services/http.js
@@ -2,6 +2,14 @@ import axios from "axios";
 
 axios.defaults.baseURL = 'http://localhost:3001';
 
+//response & requset global
+axios.interceptors.response.use((res)=>{
+    console.log(res)
+    return res;
+},(error)=>{
+    return error
+})
+
 const http={
     get:axios.get ,
     post:axios.post ,
